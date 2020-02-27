04bff8d4b4b135c86aa5c9a693fac23e699b048b
Sarah Andrew <61517073+s428465@users.noreply.github.com>
2020-02-27 13:53:09 -0500

    Update main.lua

--- AreaOfRectangleAndCircle/main.lua
+++ AreaOfRectangleAndCircle/main.lua
@@ -22,3 +22,34 @@ display.setStatusBar(display.HiddenStatusBar)
 
 -- draw a rectangle that is half the width and height of the screen size.
 myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
+-- anchor the rectangle in the top left corner of the screen and set its (x,y) position
+myRectangle.anchorX = 0
+myRectangleY = 0
+myRectangle.x = 20
+myRectangle.y = 20
+
+-- set the width of the border
+myRectangle.strokeWidth = 20
+
+-- set the colour of the rectangle
+myRectangle:setFillColor(0.7, 0.1, 0.3)
+
+-- set the color of the border
+myRectangle:serStrokeColor(0, 1, 0)
+
+-- calculate the area
+areOfRectangle = widthOfRectangle * heightOfRectangle
+
+-- write the area on the screen. Take in the size of the font when positioning it on the screen
+areaText = display.NewText(" The area of this rectangle with a of \n"..
+   widthOfRectangle .." and a height of " .. heightOfRectangle .. " is  ..
+   areaOfRectangle .." pixels².", 0,0 Arial, textSize)
+
+-- anchor the text and sit its (x,y) position
+areaText.anchorX = 0
+areaText.anchorY = 0
+areaText.x = 20
+areaText.y = display.contentHeight/2
+
+-- ser the cokour of newText
+areaText:setTextColor(1, 1, 1)
